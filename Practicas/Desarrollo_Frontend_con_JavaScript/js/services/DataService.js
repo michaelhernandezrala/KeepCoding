95fd62bbc8845c2fe4a72efb9af3a928cb9f8e83
Michael Hernandez <michaeladrian.hernandez.rala@gmail.com>
2021-12-12 21:20:28 +0100

    Pagina de Login

--- Practicas/Desarrollo_Frontend_con_JavaScript/js/services/DataService.js
+++ Practicas/Desarrollo_Frontend_con_JavaScript/js/services/DataService.js
@@ -20,5 +20,28 @@ export default {
         } catch (error) {
             throw error;
         }
+    },
+
+    authUser: async (credentials) => {
+        const url = `http://localhost:8000/auth/login`
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        };
+        try {
+            const response = await fetch(url, config);
+            const data = await response.json();
+            console.log(response)
+            if (response.ok) {
+                return data;
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 }
